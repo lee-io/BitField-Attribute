dc2ba70d80047a4da6d7b3643a974aa3b5c5432b
Lece <54936565+lee-io@users.noreply.github.com>
2019-09-06 07:00:47 +0100

    Add files via upload

---
+++ Assets/Editor/BitFieldAttributeDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(BitFieldAttribute))]
+public sealed class BitFieldAttributeDrawer : PropertyDrawer
+{
+	// Draws the enum as an Editor MaskField
+
+	public override sealed void OnGUI (Rect position, SerializedProperty property, GUIContent label)
+	{
+		if (property.propertyType == SerializedPropertyType.Enum)
+		{
+			// Ensure underlying integer support
+
+			var type = Enum.GetUnderlyingType(fieldInfo.FieldType);
+
+			if (type == typeof(SByte) ||
+				type == typeof(Int16) ||
+				type == typeof(Int32))
+			{
+				property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumDisplayNames);
+
+				return;
+			}
+		}
+
+		EditorGUI.LabelField(position, label.text, "Unsupported field type.");
+	}
+}
