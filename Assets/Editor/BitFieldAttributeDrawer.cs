aa45c81c0a22d50406c59463ba7fa27e1caec57a
Lece <54936565+lee-io@users.noreply.github.com>
2019-10-15 20:01:40 +0100

    Prefab override support + Fixed formatting

--- Assets/Editor/BitFieldAttributeDrawer.cs
+++ Assets/Editor/BitFieldAttributeDrawer.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(BitFieldAttribute))]
 public sealed class BitFieldAttributeDrawer : PropertyDrawer
 {
-	// Draws the enum as an Editor MaskField
-
+	// Draw the enum as an Editor MaskField
 	public override sealed void OnGUI (Rect position, SerializedProperty property, GUIContent label)
 	{
 		if (property.propertyType == SerializedPropertyType.Enum)
 		{
 			// Ensure underlying integer support
-
 			var type = Enum.GetUnderlyingType(fieldInfo.FieldType);
-
+			
 			if (type == typeof(SByte) ||
 				type == typeof(Int16) ||
 				type == typeof(Int32))
 			{
+				EditorGUI.BeginProperty(position, label, property);
 				property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumDisplayNames);
-
+				EditorGUI.EndProperty();
+				
 				return;
 			}
 		}
